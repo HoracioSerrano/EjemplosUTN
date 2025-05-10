d00591c3f3b2b9a87e9b6e9ec17f47a3b7341d0c
Serrano Horacio <serranohoraciohector@gmail.com>
2025-05-09 21:53:31 -0300

    Middlewares

---
+++ AppMiddlewares.js
@@ -0,0 +1,37 @@
+const express = require('express') 
+const app = express()
+const port = 3000
+
+
+function funcion1(req,res,next){
+    console.log("Dentro de funcion 1");
+    next();
+}
+function funcion2(req,res,next){
+    console.log("Dentro de funcion 2");
+    next();
+}
+function funcion3(req,res,next){
+    console.log("Dentro de funcion 3");
+    res.status(200).send("Final de cadena");
+    next();
+}
+function funcion4(req,res,next){
+    console.log("Dentro de funcion 4");
+}
+
+app.use(express.json());
+
+app.get("/mid", funcion1, funcion2, funcion3, funcion4);
+
+app.use((req,res)=>{
+    res.status(404).send("no hay nada aqui");
+});
+
+
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
