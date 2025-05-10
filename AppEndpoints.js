2b421b52b9cd47511961c8c435123fee303c773b
Serrano Horacio <serranohoraciohector@gmail.com>
2025-05-09 21:37:52 -0300

    Api

--- AppEndpoints.js
+++ AppEndpoints.js
@@ -6,15 +6,15 @@ npm install cors
 const express = require('express') 
 const app = express()
 
-app.use(express.json());
+app.use(express.json());//si viene una solicitud con header content-type aplication/json
 
 //utiliza CORS
-var cors = require('cors')
+var cors = require('cors');
 //setea cors abierto para todos los dominios
 //mas opciones de configuracion https://expressjs.com/en/resources/middleware/cors.html
-app.use(cors())//estoy seteando cors como middleware para todas las solicitudes
+app.use(cors());//estoy seteando cors como middleware para todas las solicitudes
 
-app.post('/concors',(req,res,next)=>{
+app.post('/concors', async (req,res,next)=>{
     const contenido = req.body;
     contenido.recibido="ok";
     res.status(200).send(contenido);
