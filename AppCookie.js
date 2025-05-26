bb6b92a4fa4f20beb9d5efdf9023253e1b19b775
Serrano Horacio <serranohoraciohector@gmail.com>
2025-05-26 17:27:30 -0300

    JWT y Cookies

---
+++ AppCookie.js
@@ -0,0 +1,35 @@
+/*
+Ejemplo Hola Mundo en Express
+Requiere:
+npm install cors
+*/
+const express = require('express') 
+const app = express()
+const cookieParser = require('cookie-parser');//npm install cookie-parser
+
+//utiliza CORS
+var cors = require('cors');
+//setea cors abierto para todos los dominios
+app.use(cors());//estoy seteando cors como middleware para todas las solicitudes
+app.use(cookieParser());
+
+app.get('/setCookie',(req,res,next)=>{
+    res.cookie('nombreMiCookie','valorMiCookie',{
+        httpOnly: true,
+        secure: false,          // usar solo en HTTPS
+        sameSite: 'Strict',    // o 'Lax' según tu caso
+        maxAge: 2 * 60 * 60 * 1000 // 2 horas parte de milisegundos
+    });
+    res.status(200).send('cookie seteada');
+});
+
+app.get('/readCookie',(req,res,next)=>{
+    const cookie = req.cookies.nombreMiCookie;
+    res.status(200).send(cookie);
+});
+
+
+const port = 3000
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+});
