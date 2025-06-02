3adec0aca09c1efa7baefebd646e20fc6b1c507f
Serrano Horacio <serranohoraciohector@gmail.com>
2025-06-02 20:19:17 -0300

    x

--- AppPuppeteers.js
+++ AppPuppeteers.js
@@ -9,64 +9,64 @@ app.use(express.urlencoded({ extended: true })); //const datos = req.body;
 app.use(express.json()); //const datos = req.body;
 
 class Ticket {
-  id = "";
-  fecha = "";
-  nombre = "";
-  producto = "";
-  total = 0;
-  view = true;
+    id = "";
+    fecha = "";
+    nombre = "";
+    producto = "";
+    total = 0;
+    view = true;
 }
 
 async function getTicket(req, res) {
-  const newTicket = new Ticket();
-  newTicket.id = req.params.id;
-  newTicket.fecha = new Date().toLocaleDateString();
-  newTicket.nombre = "John Doe";
-  newTicket.producto = "Sample Product";
-  newTicket.total = 100.0;
-  newTicket.view = true;
+    const newTicket = new Ticket();
+    newTicket.id = req.params.id;
+    newTicket.fecha = new Date().toLocaleDateString();
+    newTicket.nombre = "John Doe";
+    newTicket.producto = "Sample Product";
+    newTicket.total = 100.0;
+    newTicket.view = true;
 
-  let html = await ejs.renderFile(
-    path.join(__dirname, "./", "Vistas", "ticket.ejs"),
-    { ticket: newTicket }
-  );
-  res.status(200).send(html);
+    let html = await ejs.renderFile(
+        path.join(__dirname, "./", "Vistas", "ticket.ejs"),
+        { ticket: newTicket }
+    );
+    res.status(200).send(html);
 }
 
 async function downloadTicket(req, res) {
-  const newTicket = new Ticket();
-  newTicket.id = req.params.id;
-  newTicket.fecha = new Date().toLocaleDateString();
-  newTicket.nombre = "John Doe";
-  newTicket.producto = "Sample Product";
-  newTicket.total = 100.0;
-  newTicket.view = false;
-
-  let html = await ejs.renderFile(
-    path.join(__dirname, "./", "Vistas", "ticket.ejs"),
-    { ticket: newTicket }
-  );
-
-  const browser = await puppeteer.launch({
-    headless: true,
-  });
-
-  const page = await browser.newPage();
-
-  await page.setContent(html);
-
-  const pdfBuffer = await page.pdf({
-    format: "A4",
-    printBackground: true,
-    margin: {
-      top: "20px",
-      right: "20px",
-      bottom: "20px",
-      left: "20px",
-    },
-    headerTemplate: "<div>Esto es el header</div>",
-  });
-
+    const newTicket = new Ticket();
+    newTicket.id = req.params.id;
+    newTicket.fecha = new Date().toLocaleDateString();
+    newTicket.nombre = "John Doe";
+    newTicket.producto = "Sample Product";
+    newTicket.total = 100.0;
+    newTicket.view = false;
+  
+    let html = await ejs.renderFile(
+      path.join(__dirname, "./", "Vistas", "ticket.ejs"),
+      { ticket: newTicket }
+    );
+  
+    const browser = await puppeteer.launch({
+      headless: true,
+    });
+  
+    const page = await browser.newPage();
+  
+    await page.setContent(html);
+  
+    const pdfBuffer = await page.pdf({
+      format: "A4",
+      printBackground: true,
+      margin: {
+        top: "20px",
+        right: "20px",
+        bottom: "20px",
+        left: "20px",
+      },
+      headerTemplate: "<div>Esto es el header</div>",
+    });
+  
   await browser.close();
 
   res.set({
